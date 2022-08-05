f4af31f6b1e5e5a9ce85e62da8102216fdb5b38f
Damien <aubry_damien@orange.fr>
2022-08-05 21:14:49 +0200

    loader during the skills update in database

--- static/js/components/SkillsOrderManager.js
+++ static/js/components/SkillsOrderManager.js
@@ -1,10 +1,9 @@
 class SkillsOrderManager {
     constructor() {
-        this.skillsOrderContainer  = document.querySelector('.skills-order-container');
-        this.skillDivs             = document.querySelectorAll('.skill');
-        this.skillsContainers      = document.querySelectorAll('.skills-order');
-        this.skillsUpdatingMessage = document.getElementById('skills-updating');
-        this.skillsUpdatedMessage  = document.getElementById('skills-update-success');
+        this.skillDivs            = document.querySelectorAll('.skill');
+        this.skillsContainers     = document.querySelectorAll('.skills-order');
+        this.skillsOrderContainer = document.getElementById('skills-order-container');
+        this.updatingMessageDiv   = document.getElementById('updating-message');
 
         this.init();
     }
@@ -26,7 +25,8 @@ class SkillsOrderManager {
             });
 
             skillDiv.addEventListener('dragend', () => {
-                this.skillsUpdatingMessage.classList.add('active');
+                this.skillsOrderContainer.classList.add('active');
+                this.updatingMessageDiv.classList.add('active');
                 skillDiv.classList.remove('dragging');
                 this._defineElementOrderAfterDragging(skillDiv);
             });
@@ -55,37 +55,31 @@ class SkillsOrderManager {
      * @private
      */
     _defineElementOrderAfterDragging = (element) => {
-        const POSITION         = element.getElementsByTagName('span');
-        const SKILL_DIV_PARENT = element.parentNode;
-        const UNDEFINED_ORDER  = 'non défini'
+        const POSITION                                      = element.getElementsByTagName('span');
+        const SKILL_DIV_PARENT                              = element.parentNode;
+        const UNDEFINED_ORDER                               = 'non défini'
         const DIV_IS_DRAGGED_IN_THE_DEFINED_ORDER_CONTAINER = element.parentNode.classList.contains('defined-order');
 
-        // Sets the order depending on which container the element got dragged
-        if (DIV_IS_DRAGGED_IN_THE_DEFINED_ORDER_CONTAINER) {
-            console.log("en bas");
-            // Converts the parent's children into an array then we can get the position of the child in the list
-            // Adding +1 then the order starts at 1
-            // const CHILDREN_POSITION = [...(SKILL_DIV_PARENT.children)].indexOf(element) + 1
-            // POSITION[0].textContent = CHILDREN_POSITION.toString();
+        // For all the skill divs
+        [...SKILL_DIV_PARENT.children].forEach((skillDiv, i) => {
 
-            // Check if a div got its position changed in the defined order container
-            // if yes update every divs position (span)
-
-            [...SKILL_DIV_PARENT.children].forEach((skillDiv, i) => {
-                console.log("mdr");
+            // if one of them is dragged in the defined-order container
+            if (DIV_IS_DRAGGED_IN_THE_DEFINED_ORDER_CONTAINER) {
+                // assigns to all the skill divs in the container a display order
+                // if there are 3 divs and one more is dragged in, the first div
+                // will get number 1, the second one number 2 etc
                 skillDiv.getElementsByTagName('span')[0].textContent = i + 1;
-            });
-
-
-
-
-        } else {
-            console.log("en haut");
-            POSITION[0].textContent = UNDEFINED_ORDER;
-        }
-
-        // Once the spans are updated, we update the position in the DB using ajax
-        this._updateSkillOrderInDatabase(element);
+                // then update the display order in database
+                this._updateSkillOrderInDatabase(skillDiv, skillDiv.querySelector('span').textContent);
+            } else {
+                // skill div which are dragged back to the undefined-order container
+                // got its span updated by 'non défini'
+                POSITION[0].textContent = UNDEFINED_ORDER;
+                // then update the display order in database
+                // 'non défini' in front means that the display order equals to 0
+                this._updateSkillOrderInDatabase(skillDiv, '0');
+            }
+        });
     }
 
     /**
@@ -119,50 +113,27 @@ class SkillsOrderManager {
     /**
      * Updates the DB with the new position of the divs
      * @param element
+     * @param displayOrderNewValue
      * @private
      */
-    _updateSkillOrderInDatabase = (element) => {
-        let skillDivParentContainer = element.parentNode;
-        const HTTP                  = new Http();
+    _updateSkillOrderInDatabase = (element, displayOrderNewValue) => {
+        const HTTP = new Http();
 
-        // if the div is dragged into the 'defined-container' container
-        if (skillDivParentContainer.classList.contains('defined-order')) {
-            // for each div inside this container
-            [...skillDivParentContainer.children].forEach(skillDiv => {
+        // we update the current data attribute by the new path
+        // because when a skill div is dragged, its path changes
+        let updatedPath = this._updateSkillPath(element, displayOrderNewValue);
 
-                // we update the new path
-                let updatedPath = this._updateSkillPath(skillDiv, skillDiv.querySelector('span').textContent);
+        // then we build a complete URL using the new path
+        let fullUrl = this._buildFullUrl(updatedPath);
 
-                // then we build a complete URL using the new path (this._buildFullUrl(updatedPath))
-                let fullUrl = this._buildFullUrl(updatedPath);
-
-                // and we finally update the DB with the fullURL
-                HTTP.ajax({method: 'POST', url: fullUrl, async: true}).then((response) => {
-                    this.skillsUpdatingMessage.classList.remove('active');
-                    this.skillsUpdatedMessage.classList.add('active');
-                    setTimeout(() => {
-                        this.skillsUpdatedMessage.classList.remove('active');
-                    }, 2000);
-                }).catch((error) => {
-                    console.error(error.status, error.statusText);
-                });
-            });
-        } else {
-            [...skillDivParentContainer.children].forEach(skillDiv => {
-
-                let updatedPath = this._updateSkillPath(skillDiv, '0');
-                let fullUrl = this._buildFullUrl(updatedPath);
-
-                HTTP.ajax({method: 'POST', url: fullUrl, async: true}).then((response) => {
-                    this.skillsUpdatingMessage.classList.remove('active');
-                    this.skillsUpdatedMessage.classList.add('active');
-                    setTimeout(() => {
-                        this.skillsUpdatedMessage.classList.remove('active');
-                    }, 2000);
-                }).catch((error) => {
-                    console.error(error.status, error.statusText);
-                });
-            });
-        }
+        // and we finally update the DB with the fullURL
+        HTTP.ajax({method: 'POST', url: fullUrl, async: true}).then((response) => {
+            setTimeout(() => {
+                this.skillsOrderContainer.classList.remove('active');
+                this.updatingMessageDiv.classList.remove('active');
+            }, 1000);
+        }).catch((error) => {
+            console.error(error.status, error.statusText);
+        });
     }
 }
