096a78e9627cc75c2a6861405af7022feb6d6b1b
Damien <aubry_damien@orange.fr>
2022-08-20 19:04:08 +0200

    refactoring + portfolio + slider

--- static/js/components/Slider.js
+++ static/js/components/Slider.js
@@ -5,9 +5,8 @@ class Slider {
         this.nextSlideBtn     = this.body.querySelector('#next-slide');
         this.slider           = this.body.querySelector('.slider');
         this.sliderImages     = this.slider.querySelectorAll('img');
-        this.translation      = 300;
-        this.position         = 0;
-        this.direction = {
+        this.dotsNavigation   = this.body.querySelectorAll('.dot');
+        this.direction        = {
             previous: 'previous',
             next: 'next'
         }
@@ -21,29 +20,52 @@ class Slider {
     }
 
     _slide = () => {
+        // previous btn
         this.previousSlideBtn.addEventListener('click', () => {
+            // clones the last slider image and puts it right before the current one
+            // and then deletes the one which got cloned
             this._createSlide(this.direction.previous);
-            this.sliderImages.forEach(sliderImage => {
-                if(sliderImage.classList.contains('active')) {
-                       sliderImage.classList.remove('active');
-                }
+            // hides all the images
+            this._hideSliderImages();
+            // shows the current image
+            this._initSlide().then((resolve) => {
+                // after showing them, inits the corresponding dot
+                this._initDotsNavigation();
             });
-            this._initSlide();
         });
 
+        // next btn
         this.nextSlideBtn.addEventListener('click', () => {
+            // clones the first slider image and puts it at the end of the slider
+            // and then deletes the one which got cloned
             this._createSlide(this.direction.next);
-            this.sliderImages.forEach(sliderImage => {
-                if(sliderImage.classList.contains('active')) {
-                    sliderImage.classList.remove('active');
-                }
+            // hides all the images
+            this._hideSliderImages();
+            // shows the current image
+            this._initSlide().then((resolve) => {
+                // after showing them, inits the corresponding dot
+                this._initDotsNavigation();
             });
-            this._initSlide();
+        });
+
+        // dots navigation
+        this.dotsNavigation.forEach((dot, i) => {
+            dot.addEventListener('click', () => {
+                // hides all the images
+                this._hideSliderImages();
+                // only shows the one we want
+                this.sliderImages[i].classList.add('active');
+            });
         });
     }
 
     _initSlide = () => {
-        this.sliderImages[0].classList.add('active');
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                this.sliderImages[0].classList.add('active');
+                resolve(true);
+            }, 100)
+        });
     }
 
     _createSlide = (direction) => {
@@ -59,7 +81,7 @@ class Slider {
     }
 
     _createPreviousSlide = () => {
-        const LAST_SLIDE = this.sliderImages.length - 1;
+        const LAST_SLIDE         = this.sliderImages.length - 1;
         const NEW_PREVIOUS_SLIDE = this.sliderImages[LAST_SLIDE].cloneNode(true);
 
         this.slider.prepend(NEW_PREVIOUS_SLIDE);
@@ -69,7 +91,7 @@ class Slider {
     }
 
     _createNextSlide = () => {
-        const FIRST_SLIDE = 0;
+        const FIRST_SLIDE    = 0;
         const NEW_LAST_SLIDE = this.sliderImages[FIRST_SLIDE].cloneNode(true);
 
         this.slider.append(NEW_LAST_SLIDE);
@@ -79,6 +101,27 @@ class Slider {
     }
 
     _updateSliderImages = () => {
-        this.sliderImages     = this.slider.querySelectorAll('img');
+        this.sliderImages = this.slider.querySelectorAll('img');
+    }
+
+    _hideSliderImages = () => {
+        this.sliderImages.forEach(sliderImage => {
+            if (sliderImage.classList.contains('active')) {
+                sliderImage.classList.remove('active');
+            }
+        });
+    }
+
+    _initDotsNavigation = () => {
+        this.sliderImages.forEach((sliderImage, i) => {
+            this.dotsNavigation[i].classList.remove('active');
+            setTimeout(() => {
+                // when an image is active
+                if (sliderImage.classList.contains('active')) {
+                    // activates the corresponding dot
+                    this.dotsNavigation[sliderImage.dataset.slideNumber - 1].classList.add('active');
+                }
+            }, 100)
+        });
     }
 }
